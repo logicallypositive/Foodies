ee292ef14e71259678420e9d5267c44bbbb78e25
logically positive <brianizquierdo0229@gmail.com>
2023-11-17 12:17:25 -0500

    fixed issue

--- API/Program.cs
+++ API/Program.cs
@@ -5,12 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 //Services
 builder.Services.AddControllers();
-var app = builder.Build();
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+var app = builder.Build();
 
 // Middleware
 app.UseHttpsRedirection();
