90f0f405baee481aa61fb3cd25cdb190b13bd6dd
logically positive <brianizquierdo0229@gmail.com>
2023-11-17 16:13:28 -0500

    added axios to call api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import axios from 'axios';
 
 function App() {
+  const [foods, setFoods] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/foods')
+      .then(response => {
+        setFoods(response.data)
+      })
+  },[])
 
   return (
-    <h1>We are foodies!</h1>
+    <div>
+      <h1>We are foodies!</h1>
+
+      <ul>
+        {foods.map((food: any) => (
+          <li key={food.id}>
+            {food.name}
+          </li>
+        ))}
+      </ul>
+
+    </div>
   )
 }
 
