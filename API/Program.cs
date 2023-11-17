e0db49e33b4b067d7625c9c9f80c3bbbc06c8cdc
logically positive <brianizquierdo0229@gmail.com>
2023-11-17 11:35:19 -0500

    Added DataContext

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Services
 builder.Services.AddControllers();
 var app = builder.Build();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 
 // Middleware
